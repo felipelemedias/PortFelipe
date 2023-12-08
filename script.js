d0d2e623012284cfbe52707ed3dbab8beeb3b511
felipelemedias <flemedias@gmail.com>
2023-12-08 03:07:50 -0300

    upando portifolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["a Web Designer", "a Student", "an Engineer", "a Designer", "INSANE"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
